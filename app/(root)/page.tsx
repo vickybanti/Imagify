3f2f7541f1816149da1720e7e1dfd06756517de1
VICTOR <olamuyiwavictor55@gmail.com>
2025-01-08 19:59:06 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { SignInButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      
+      </div>
+  )
+}
+
+export default Home
