f3b71f5723f2eac9b3016f34388a68d88240aab5
VICTOR <olamuyiwavictor55@gmail.com>
2025-01-08 21:50:03 +0100

    implemeting clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,5 @@
 /* eslint-disable camelcase */
-import {clerkClient } from '@clerk/clerk-sdk-node';
-import { WebhookEvent } from "@clerk/nextjs/server";
+import { WebhookEvent,clerkClient } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
@@ -71,10 +70,12 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
+    const client = await clerkClient()
+
 
     // Set public metadata
     if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      await client.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
