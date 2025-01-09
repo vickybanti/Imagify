91dfc1062db2b78b4730a478426fa9f215a142a7
VICTOR <olamuyiwavictor55@gmail.com>
2025-01-09 10:44:28 +0100

    changed mongodb connection

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,30 +1,21 @@
-import mongoose, {Mongoose} from 'mongoose'
+import mongoose from 'mongoose'
 
+const MONGODB_URI = process.env.MONGODB_URI
 
-const MONGODB_URL = process.env.MONGODB_URL;
+let cached = (global as any).mongoose || {conn:null, promise:null};
 
-interface MongooseConnection {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null
+export const connectToDb = async () => {
+    if (cached.conn) return cached.conn
+
+    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing ');
+
+    cached.promise = await cached.promise || mongoose.connect(MONGODB_URI,{
+        dbName:'imagify',
+        bufferCommands:false
+    })
+
+    cached.conn = await cached.promise
+
+    return cached.conn;
 }
 
-    let cached: MongooseConnection = (global as any).mongoose
-    if(!cached) {
-        cached = (global as any).mongoose = {
-            conn: null, promise:null
-        }
-    }
-
-    export const connectToDatabase = async () => {
-        if(cached.conn) return cached.conn;
-
-        if(!MONGODB_URL) throw new Error('Missing MONGODB_URL')
-
-        cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-            dbName:'imaginify', bufferCommands:false
-        })
-
-        cached.conn= await cached.promise
-
-        return cached.conn;
-    }
