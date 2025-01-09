1c2b605e4eb6a453a9295bb43236dfcf33140774
VICTOR <olamuyiwavictor55@gmail.com>
2025-01-09 15:54:16 +0100

    changed mongodb connection

--- lib/database/models/transaction.model.ts
+++ lib/database/models/transaction.model.ts
@@ -1,5 +1,19 @@
 import { Schema, model, models } from "mongoose";
 
+export interface ITransaction extends Document {
+  createdAt?: Date; // Optional since it has a default value
+  stripeId: string;
+  amount: number;
+  plan?: string; // Optional since it's not marked as required
+  credits?: number; // Optional since it's not marked as required
+  buyer?:  {
+    _id:string;
+    firstName:string;
+    lastName:string;
+}  // Referencing a User by ObjectId (assumed to be a string)
+}
+
+
 const TransactionSchema = new Schema({
   createdAt: {
     type: Date,
--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,6 +1,21 @@
+
 import { model, models, Schema } from "mongoose";
 
+export interface IUser extends Document{
+    _id?: string; // Optional since it's not marked as required in the schema
+    clerkId: string;
+    email: string;
+    username: string;
+    photo: string;
+    firstName: string;
+    lastName: string;
+    planId?: number; // Optional since it has a default value
+    creditBalance?: number; // Optional since it has a default value
+  }
+  
+
 const UserSchema = new Schema({
+    _id:{type:String},
     clerkId:{type:String, required:true, unique:true},
     email:{type:String, required:true, unique:true},
     username:{type:String,required:true, unique:true},
@@ -12,6 +27,6 @@ const UserSchema = new Schema({
     
 });
 
-const User = models?.User || model('User', UserSchema)
+const User = models.User || model('User', UserSchema);
 
 export default User;
